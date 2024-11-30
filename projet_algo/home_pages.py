a876bedd1676a6ad1094d651866ce261ff8d2484
hamdane1548 <oussamahamdane80@gmail.com>
2024-11-30 10:54:58 +0100

    nv modification ajouter les carte pas encore termine mais j'ai insialise les places de les cartes

--- projet_algo/home_pages.py
+++ projet_algo/home_pages.py
@@ -57,6 +57,50 @@ class home_pages(QMainWindow):
        self.about_texte=QLabel("About",self)
        self.about_texte.setGeometry(420,20,100,30)
        self.about_texte.setStyleSheet("color:white;""font-size:15px;""font-family:italic;")
+       #pages principal
+       self.full_texte="Bienvenu dans AlgoTest : Dijkstra, Tas de Fibonacci et B-Arbres 😊"
+       self.current_texte=""
+       self.index=0
+       self.label_titre=QLabel("",self)
+       self.label_titre.setGeometry(410,100,800,40)
+       self.label_titre.setStyleSheet("color:black;font-size:26px;font-family:'Roboto';")
+       self.timer=QTimer()
+       self.timer.timeout.connect(self.updatetexte)
+       self.timer.start(110)
+       #carte de prsente les algorihtme
+       #square
+       #carte 1 de Dijisktre
+       self.carte=QLabel(self)
+       self.carte.setStyleSheet("background:#8f9ed1; border:solid 2px #a5a29a ;border-radius:15px")
+       self.carte.setGeometry(400,270,220,220)
+       self.image_dijkstra=QPixmap("projet/dijkstra.png")
+       self.dijkstra_label=QLabel(self)
+       self.dijkstra_label.setPixmap(self.image_dijkstra)
+       self.dijkstra_label.setGeometry(477,280,75,75)
+       self.dijkstra_label.setScaledContents(True)
+       self.titre_dijkstra=QLabel("Algo Dijikstra",self)
+       self.titre_dijkstra.setGeometry(458,370,200,30)
+       self.titre_dijkstra.setStyleSheet("color:black;font-size:18px;font-family:'Roboto'")
+       #---------------------------------------------
+       self.carte2=QLabel(self)
+       self.carte2.setStyleSheet("background:#a5a29a; border:solid 2px #a5a29a ;border-radius:15px")
+       self.carte2.setGeometry(700,270,220,220)
+       self.carte3=QLabel(self)
+       self.carte3.setStyleSheet("background:#a587c5; border:solid 2px #a5a29a ;border-radius:15px")
+       self.carte3.setGeometry(1000,270,220,220)
+       qhbox=QHBoxLayout()
+       qhbox.addWidget(self.carte)
+       qhbox.addWidget(self.carte2)
+       qhbox.addWidget(self.carte3)
+       self.setLayout(qhbox)
+    def updatetexte(self):
+        if self.index<len(self.full_texte):
+            self.current_texte+=self.full_texte[self.index]
+            self.label_titre.setText(self.current_texte)
+            self.index+=1
+        else:
+            self.timer.stop()
+        
     def dashbord(self):
         self.central=QWidget()
         self.setCentralWidget(self.central)
@@ -65,7 +109,7 @@ class home_pages(QMainWindow):
         self.layout.addWidget(self.mon_dashbord)
         self.layout.setContentsMargins(0,0,0,0)
         self.central.setLayout(self.layout)
-
+     
 def main():
     app=QApplication(sys.argv)
     window=home_pages()
